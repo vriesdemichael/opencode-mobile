9343978d2367f6759cb0313a35fd3ac13508523a
Michael de Vries <vriesdemichael@gmail.com>
2026-02-20 00:25:44 +0100

    fix: replace process.env.EXPO_OS with Platform.OS for typecheck

--- components/external-link.tsx
+++ components/external-link.tsx
@@ -4,6 +4,7 @@ import {
 	WebBrowserPresentationStyle,
 } from "expo-web-browser";
 import type { ComponentProps } from "react";
+import { Platform } from "react-native";
 
 type Props = Omit<ComponentProps<typeof Link>, "href"> & {
 	href: Href & string;
@@ -16,7 +17,7 @@ export function ExternalLink({ href, ...rest }: Props) {
 			{...rest}
 			href={href}
 			onPress={async (event) => {
-				if (process.env.EXPO_OS !== "web") {
+				if (Platform.OS !== "web") {
 					// Prevent the default behavior of linking to the default browser on native.
 					event.preventDefault();
 					// Open the link in an in-app browser.
